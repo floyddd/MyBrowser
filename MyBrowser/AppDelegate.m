90e580b877ba3185097d92ce2cb5262e9c611bfb
floyddd <floydparag@gmail.com>
2014-03-27 20:02:24 +0545

    Bug fixes

--- MyBrowser/AppDelegate.m
+++ MyBrowser/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"launching with options");
     _bookmarksArray = [[NSMutableArray alloc]initWithObjects:@"www.facebook.com",@"www.twitter.com",nil];
     _historyArray = [[NSMutableArray alloc] init];
     NSString *myPath = [self saveFilePath];
@@ -29,7 +30,7 @@
     NSMutableArray *b = [[NSMutableArray alloc] initWithContentsOfFile:myPath];
     _bookmarksArray=b;
         
-       
+   
     }
 
     return YES;
@@ -37,13 +38,14 @@
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
+   
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    
+  NSLog(@"enter background");
     NSArray *values = [[NSArray alloc] initWithArray:self.bookmarksArray];
 	[values writeToFile:[self saveFilePath] atomically:YES];
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
@@ -59,6 +61,7 @@
 }
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
+    
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
@@ -69,8 +72,9 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-   
-
+     NSLog(@"terminate");
+    NSArray *values = [[NSArray alloc] initWithArray:self.bookmarksArray];
+	[values writeToFile:[self saveFilePath] atomically:YES];
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
